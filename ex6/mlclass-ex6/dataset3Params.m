05cf3742ededf49e6a04b8cb1a8d0a3036df1561
Louis Salin <louis.phil@gmail.com>
2011-11-22 18:14:56 -0600

    completed ex6 code

--- ex6/mlclass-ex6/dataset3Params.m
+++ ex6/mlclass-ex6/dataset3Params.m
@@ -23,11 +23,30 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+testC     = [0.01 0.03 0.1 0.3 1 3 10 30];
+testSigma = [0.01 0.03 0.1 0.3 1 3 10 30];
+previousError = 10000;
 
+results = zeros(64, 3);
 
+for i=1:8
+	for j=1:8
+		c = testC(i);
+		s = testSigma(j);
 
+		model = svmTrain(X, y, c, @(x1, x2) gaussianKernel(x1, x2, s));
+		predictions = svmPredict(model, Xval);
+		newError = mean(double(predictions ~= yval));
 
+		results((i-1)*8 + j, :) = [c s newError];
 
+		if newError < previousError
+			C = c;
+			sigma = s;
+			previousError = newError;
+		end
+	end
+end
 
 % =========================================================================
 
